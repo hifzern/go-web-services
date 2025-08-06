22fe4fe9ae18f43bbdcc5d6f247e8bc2c99e9523
adh1ka <hafizhadhi27@gmail.com>
2025-08-06 23:18:46 +0700

    done tested

--- 01-HelloWorld/main.go
+++ 01-HelloWorld/main.go
@@ -1,1 +1,29 @@
-package _1_HelloWorld
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	var message = "Welcome"
+	w.Write([]byte(message))
+}
+
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+	var message = "Hello World"
+	w.Write([]byte(message))
+}
+
+func main() {
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/index", handlerIndex)
+	http.HandleFunc("/hello", handlerHello)
+
+	var address = "localhost:1337"
+	fmt.Printf("server started at %s \n", address)
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
