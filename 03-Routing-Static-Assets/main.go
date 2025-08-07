dbfa01d26a13cd90d05382681d73c3c7d0c064ac
adh1ka <hafizhadhi27@gmail.com>
2025-08-07 09:59:48 +0700

    done tested

---
+++ 03-Routing-Static-Assets/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "net/http"
+
+func main() {
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("assets"))))
+	fmt.Println("server started at localhost:9000 \n")
+	http.ListenAndServe(":9000", nil)
+}
