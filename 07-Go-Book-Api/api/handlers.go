b6f4f77fd89f8da8090d7de9d5589095c124e8de
hifzern <hafizhadhi27@gmail.com>
2025-08-17 09:38:12 +0700

    create func /api/handlers.go

---
+++ 07-Go-Book-Api/api/handlers.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to connect to database : ", err)
+	}
+
+	dsn := os.Getenv("DB_URL")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database : ", err)
+	}
+
+	//migrate the schema
+	if err := DB.AutoMigrate(&Book{}); err != nil {
+		log.Fatal("Failed to migrate schema : ", err)
+	}
+}
+
+func CreateBook(c *gin.Context) {
+	var book Book
+
+	//bind the request body
+	if err := c.ShouldBindJSON(&book); err != nil {
+		ResponseJSON(c, http.StatusBadRequest, "Invalid Input", nil)
+		return
+	}
+
+	DB.Create(&book)
+	ResponseJSON(c, http.StatusCreated, "Book created sucessfully", book)
+}
