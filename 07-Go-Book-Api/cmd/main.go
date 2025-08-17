6767b0e62f16d189d10799a6eca8ad817b2286af
hifzern <hafizhadhi27@gmail.com>
2025-08-17 10:30:24 +0700

    completing

--- 07-Go-Book-Api/cmd/main.go
+++ 07-Go-Book-Api/cmd/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"07-Go-Book-Api/api"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	api.InitDB()
+	r := gin.Default()
+
+	//routes
+	r.POST("/book", api.CreateBook)
+	r.GET("/books", api.GetBooks)
+	r.GET("/book/:id", api.GetBook)
+	r.PUT("/book/:id", api.UpdateBook)
+	r.DELETE("/book/:id", api.DeleteBook)
+
+	r.Run(":8080")
+}
